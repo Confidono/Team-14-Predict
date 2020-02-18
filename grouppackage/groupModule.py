464a162906ad8b702282c242740a483dbaa8c646
ImanDS <thekwena011@gmail.com>
2020-02-18 21:50:20 +0200

    added unittests for first three functions

--- grouppackage/groupModule.py
+++ grouppackage/groupModule.py
@@ -27,3 +27,26 @@ def dictionary_of_metrics(items):
                    'max': np.max(np_list).round(2),} #create a dictionary that calculates the five metrics
  
     return metric_dict #return result as a dictionary
+
+
+def five_num_summary(items):
+    """This function takes in a list of integers and returns a dictionary of the five number summary"""
+    np_list = np.array(items)
+    metric_dict = {'max': np.max(np_list).round(2),
+                   'median': round(np.median(np_list), 2),
+                   'min': np.min(np_list).round(2),
+                   'q1': np.quantile(np_list, 0.25).round(2),
+                   'q3': np.quantile(np_list, 0.75).round(2),
+                   }
+    
+    return metric_dict
+
+def date_parser(dates):
+    """This function takes a list of strings with date and time then
+    it returns a string with only the date
+    """
+    new_dates = []
+    for i in range(len(dates)):
+        a = dates[i][:10]        
+        new_dates.append(a)
+    return new_dates
--- grouppackage/test_func.py
+++ grouppackage/test_func.py
@@ -2,12 +2,20 @@ import unittest
 import groupModule
 
 class TestFuncs(unittest.TestCase):
-
+    '''
+    Testing dictionary_of_metrics function
+    '''
     def test_dictionary_of_metrics(self):
         self.assertEqual(groupModule.dictionary_of_metrics([39660, 36024, 32127, 39488, 18422]), {'mean':33144.2 , 'median': 36024.0, 'var': 77192641.2, 'std': 8785.93, 'min': 18422, 'max': 39660})
+        self.assertEqual(groupModule.dictionary_of_metrics([51860, 68121, 49881, 42034, 54646]), {'mean': 53308.4, 'median': 51860.0, 'var': 90539830.3, 'std': 9515.24, 'min': 42034, 'max': 68121})
 
-
-
+    def test_five_num_summary(self):
+        self.assertEqual(groupModule.five_num_summary([39660, 36024, 32127, 39488, 18422]), {'max': 39660, 'median': 36024.0, 'min': 18422, 'q1': 32127.0, 'q3': 39488.0})
+        self.assertEqual(groupModule.five_num_summary([51860, 68121, 49881, 42034, 54646]), {'max': 68121, 'median': 51860.0, 'min': 42034, 'q1': 49881.0, 'q3': 54646.0})
+   
+    def test_date_parser(self):
+        self.assertEqual(groupModule.date_parser(['2019-11-29 12:50:54','2019-11-29 12:46:53','2019-11-29 12:46:10']),['2019-11-29', '2019-11-29', '2019-11-29'] )
+        self.assertEqual(groupModule.date_parser(['2020-11-29 12:50:54','2019-11-20 12:46:53','2007-07-07 12:46:10']), ['2020-11-29','2019-11-20','2007-07-07'])
 
 if __name__ == '__main__':
     unittest.main()
